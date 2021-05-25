94d508addbb35a9b5acd8e43ab01f5202fc60b3a
Kerolos Atef Saber <54465159+0xWise64@users.noreply.github.com>
2021-05-25 06:25:46 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>SSRF Listener</title>
+    <link rel="stylesheet" href="styles.css">
+
+	<script src=
+"https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js">
+	</script>
+	<script src="/script.js"></script>
+
+</head>
+<body>
+
+	<center>
+		<h1>SSRF Listener Script coded by Kerolos Atef AKA 0xWise, 3x3s.</h1>
+		<h3 id="vuln"></h3>
+        <p id="ip"></p>
+	</center>
+
+<?php include 'log.php';?>
+
+</body>
+</html>
