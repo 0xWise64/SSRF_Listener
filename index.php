ce1fa805c8854d6059b04c00a9ddf88ccb552da6
Kerolos Atef Saber <54465159+0xWise64@users.noreply.github.com>
2021-05-28 07:43:15 +0200

    Update index.php
    Add "/" in href to the style sheet to set it to the root path, because when calling the SSRF Listener from a 2nd directory the style sheet is not loaded.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <head>
 <title>SSRF Listener</title>
-<link rel="stylesheet" href="styles.css">
+<link rel="stylesheet" href="/styles.css">
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="/script.js"></script>
@@ -12,7 +12,7 @@
 <body>
 
 <center>
-	<h1>SSRF Listener Script coded by Kerolos Atef AKA 0xWise, 3x3s.</h1>
+	<h1>Your SSRF Listener Script coded by Kerolos Atef AKA 0xWise, 3x3s.</h1>
 	<h3 id="vuln"></h3>
         <p id="ip"></p>
 </center>
